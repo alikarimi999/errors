c04d5fca11b00c17d8f38e63584289be7ddee86c
ali <alikarimi999@protonmail.com>
2022-07-16 16:46:14 +0430

    first implementaion

---
+++ error.go
@@ -0,0 +1,114 @@
+package errors
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type ErrCode string
+
+const (
+	ErrNotFound         = ErrCode("not found")
+	ErrForbidden        = ErrCode("forbidden")
+	ErrConflict         = ErrCode("conflict")
+	ErrConnectionFailed = ErrCode("connection failed")
+	ErrInternal         = ErrCode("internal")
+	ErrBadRequest       = ErrCode("bad request")
+	ErrUnknown          = ErrCode("unknown error")
+)
+
+type Op string // Operation name
+type Msg interface {
+	String() string
+}
+
+type Error struct {
+	code    ErrCode
+	message Msg
+
+	op              Op
+	err             error
+	relationContext string
+}
+
+func Wrap(args ...interface{}) error {
+	if len(args) == 0 {
+		return nil
+	}
+
+	e := &Error{}
+	for _, arg := range args {
+		switch arg := arg.(type) {
+		case ErrCode:
+			e.code = arg
+		case Msg:
+			e.message = arg
+		case Op:
+			e.op = arg
+		case error:
+			e.err = arg
+		case string:
+			e.relationContext = arg
+		default:
+			continue
+		}
+	}
+	return e
+}
+
+func ErrorCode(err error) ErrCode {
+	if err == nil {
+		return ""
+	} else if e, ok := err.(*Error); ok && e.code != "" {
+		return e.code
+	} else if ok && e.err != nil {
+		return ErrorCode(e.err)
+	}
+
+	return "unknoun error"
+}
+
+func ErrorMsg(err error) string {
+	if err == nil {
+		return ""
+	} else if e, ok := err.(*Error); ok && e.message != nil {
+		return e.message.String()
+	} else if ok && e.err != nil {
+		return ErrorMsg(e.err)
+	}
+	return "An internal error has occurred."
+}
+
+func (e *Error) Error() string {
+	var buf bytes.Buffer
+
+	// Print the current operation in our stack, if any.
+	if e.op != "" {
+		fmt.Fprintf(&buf, "%s: ", e.op)
+	}
+
+	// If wrapping an error, print its Error() message.
+	// Otherwise print the error code & message.
+	if e.err != nil {
+		buf.WriteString(e.err.Error())
+		if e.relationContext != "" {
+			fmt.Fprintf(&buf, " (%s)", e.relationContext)
+		}
+	} else {
+		if e.code != "" {
+			fmt.Fprintf(&buf, "<%s> ", e.code)
+		}
+		buf.WriteString(e.message.String())
+	}
+	return buf.String()
+}
+func ErrorOp(e error) Op {
+	if e == nil {
+		return ""
+	} else if e, ok := e.(*Error); ok && e.op != "" {
+		return e.op
+	} else if ok && e.err != nil {
+		return ErrorOp(e.err)
+	}
+	return ""
+}
